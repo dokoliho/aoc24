aa6d85e8281b4844767cd88719b9893f86e8968f
ohofmann <oliver@dokoliho.de>
2024-12-16 05:58:45 +0100

    Day 16

---
+++ day16_solution.py
@@ -0,0 +1,20 @@
+from aoc_tools.solution import Solution
+
+class D16S(Solution):
+
+    def __init__(self):
+        super().__init__()
+        self.day = 16
+        self.expected_test_result_part_1 = 1
+        self.expected_test_result_part_2 = 2
+
+    def solve_part_1(self):
+        return 1
+
+    def solve_part_2(self):
+        return 2
+
+
+if __name__ == "__main__":
+    D16S().test()
+    D16S().solve()
