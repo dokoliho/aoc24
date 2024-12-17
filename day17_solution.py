b939e60fd056110bf76de26b41d31926353fbde9
ohofmann <oliver@dokoliho.de>
2024-12-17 15:13:30 +0100

    Day 17: First dirty solution

--- day17_solution.py
+++ day17_solution.py
@@ -1,37 +1,35 @@
 from aoc_tools.solution import Solution
 
-screen = []
 
 def adv(a, b, c, operand, pc):
     operand = combo(a, b, c, operand)
-    return (a >> operand, b, c, pc+2)
+    return (a >> operand, b, c, pc+2, None)
 
 def bxl(a, b, c, operand, pc):
-    return (a, b ^ operand, c, pc+2)
+    return (a, b ^ operand, c, pc+2, None)
 
 def bst(a, b, c, operand, pc):
     operand = combo(a, b, c, operand)
-    return (a, operand & 0b111, c, pc+2)
+    return (a, operand & 0b111, c, pc+2, None)
 
 def jnz(a, b, c, operand, pc):
-    return (a, b, c, pc+2 if a == 0 else operand)
+    return (a, b, c, pc+2 if a == 0 else operand, None)
 
 def bxc(a, b, c, _, pc):
-    return (a, b ^ c, c, pc+2)
+    return (a, b ^ c, c, pc+2, None)
 
 def out(a, b, c, operand, pc):
     global screen
     operand = combo(a, b, c, operand)
-    screen.append(operand & 0b111)
-    return (a, b, c, pc+2)
+    return (a, b, c, pc+2, operand & 0b111)
 
 def bdv(a, b, c, operand, pc):
     operand = combo(a, b, c, operand)
-    return (a, a >> operand, c, pc+2)
+    return (a, a >> operand, c, pc+2, None)
 
 def cdv(a, b, c, operand, pc):
     operand = combo(a, b, c, operand)
-    return (a, b, a >> operand, pc+2)
+    return (a, b, a >> operand, pc+2, None)
 
 def combo(a, b, c, operand):
     if operand == 4:
@@ -44,6 +42,7 @@ def combo(a, b, c, operand):
         raise ValueError("Invalid operand")
     else:
         return operand
+
 
 
 COMMANDS = [adv, bxl, bst, jnz, bxc, out, bdv, cdv]
@@ -61,17 +60,30 @@ class D17S(Solution):
         self.instructions = None
 
     def solve_part_1(self):
-        global screen
         self.get_initial_values()
+        screen =  self.run_instructions(self.a, self.b, self.c, self.instructions)
+        return ",".join(map(str, screen))
+
+
+    def run_instructions(self, a, b, c, instructions):
         pc = 0
         screen = []
         while pc < len(self.instructions):
-            command = COMMANDS[self.instructions[pc]]
-            self.a, self.b, self.c, pc = command(self.a, self.b, self.c, self.instructions[pc+1], pc)
-        return ",".join(map(str, screen))
+            op_code = self.instructions[pc]
+            command = COMMANDS[op_code]
+            a, b, c, pc, out = command(a, b, c, instructions[pc + 1], pc)
+            if out is not None:
+                screen.append(out)
+        return screen
 
     def solve_part_2(self):
-        global screen
+        if self.is_test:
+            result = self.solve_part_2_test()
+        else:
+            result = self.solve_part_2_real()
+        return result
+
+    def solve_part_2_test(self):
         self.puzzle = [
             "Register A: 2024",
             "Register B: 0",
@@ -80,20 +92,57 @@ class D17S(Solution):
             "Program: 0, 3, 5, 4, 3, 0",
         ]
         self.get_initial_values()
-        a = 0
-        while True:
-            pc = 0
-            screen = []
-            b, c = self.b, self.c
-            while pc < len(self.instructions):
-                command = COMMANDS[self.instructions[pc]]
-                a, b, c, pc = command(a, b, c, self.instructions[pc+1], pc)
-                if not all(a == b for a, b in zip(screen, self.instructions)):
+        result = self.find_valid_a(0, [], 0)
+        print(self.run_instructions(result, self.b, self.c, self.instructions))
+
+        #        result = 0
+#        for instruction in reversed(self.instructions):
+#            result = (result << 3) + instruction
+#        result = result << 3
+#        print(self.run_instructions(result, self.b, self.c, self.instructions))
+        return result
+
+    def solve_part_2_real(self):
+        self.get_initial_values()
+        result =  self.find_valid_a(0, [], 0)
+        print(self.run_instructions(result, self.b, self.c, self.instructions))
+        return result
+
+    def find_valid_a(self, fixed, presets, instruction_index):
+        if instruction_index == len(self.instructions):
+            return fixed
+        if instruction_index > len(self.instructions):
+            return None
+        if instruction_index == 1:
+            pass
+        for candidate in range(1024):
+            candidate_bits = [int(bit) for bit in format(candidate, f'010b')]
+            aligned_with_presets = True
+            for i in range(-1, -len(presets), -1):
+                if candidate_bits[i] != presets[i]:
+                    aligned_with_presets = False
                     break
-            if screen == self.instructions:
-                return a
-            a = a + 1
-            if (a%100) == 0: print(a)
+            if not aligned_with_presets:
+                continue
+            test_value = (candidate << (instruction_index * 3)) + fixed
+            screen = self.run_instructions(test_value, self.b, self.c, self.instructions)
+            if len(screen) < instruction_index + 1:
+                continue
+            screen_ok = True
+            for i in range(instruction_index+1):
+                if screen[i] != self.instructions[i]:
+                    screen_ok = False
+                    break
+            if not screen_ok:
+                continue
+            fixed_part = candidate % 8
+            fixed_part = fixed_part << (instruction_index * 3)
+            new_fixed = fixed + fixed_part
+            new_presets = [int(bit) for bit in format(candidate >> 3, f'07b')]
+            result = self.find_valid_a(new_fixed, new_presets, instruction_index + 1)
+            if result is not None:
+                return result
+        return None
 
     def get_initial_values(self):
         self.a = int(self.puzzle[0].split(":")[1])
